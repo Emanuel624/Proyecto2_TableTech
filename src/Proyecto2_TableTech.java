1fed59628fc7b9d699200d1600d34dc9dce1ffa3
Emanuel624 <emanuel.chavarria.hernandez@gmail.com>
2023-04-25 00:40:48 -0600

    Se agrega el Proyecto #2

---
+++ src/Proyecto2_TableTech.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Proyecto2_TableTech {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Ingrese su nombre: ");
+        String nombre = scanner.nextLine();
+        System.out.println("Hola " + nombre + "!");
+    }
+}
